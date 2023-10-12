7bbe3e944ea0a6003411b8cc13e54c05b043abda
Max Kotyk <mxm.kotyk@gmail.com>
2023-10-12 20:35:28 +0200

    Added auth router to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import contactsRouter from "./routes/api/index.js";
+import { contactsRouter, authRouter } from "./routes/api/index.js";
 
 dotenv.config();
 
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
