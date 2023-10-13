959705f1a8f8868b8f786f5cccbd435af3b30b9d
Max Kotyk <mxm.kotyk@gmail.com>
2023-10-13 13:59:38 +0200

    Removed console.log

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  console.log(err);
   res
     .status(err.status || 500)
     .json({ message: err.message || "Server error" });
