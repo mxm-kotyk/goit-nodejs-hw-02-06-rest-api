8f56b9ceb29b3509853443ea01d508a103fa2c33
Max Kotyk <mxm.kotyk@gmail.com>
2023-10-13 16:27:11 +0200

    Added login controller, router and token generator

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -3,21 +3,24 @@ import express from "express";
 import { authController } from "../../controllers/index.js";
 import { emptyBodyCheck } from "../../middlewares/index.js";
 import { validateBody } from "../../decorators/index.js";
-import {
-  userSignInValidationSchema,
-  userSignUpValidationSchema,
-} from "../../schemas/index.js";
+import { userValidationSchema } from "../../schemas/index.js";
 
-const validateSignUpUser = validateBody(userSignUpValidationSchema);
-const validateSignInUser = validateBody(userSignInValidationSchema);
+const validateUser = validateBody(userValidationSchema);
 
 const authRouter = express.Router();
 
 authRouter.post(
   "/register",
   emptyBodyCheck,
-  validateSignUpUser,
+  validateUser,
   authController.registerUser
+);
+
+authRouter.post(
+  "/login",
+  emptyBodyCheck,
+  validateUser,
+  authController.loginUser
 );
 
 export default authRouter;
