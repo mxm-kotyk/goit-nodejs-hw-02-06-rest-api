b202c43ce0c76f80ac3886c9ecbc017e85203f0d
Max Kotyk <mxm.kotyk@gmail.com>
2023-10-13 16:26:19 +0200

    Added password hashing

--- controllers/auth/registerUser.js
+++ controllers/auth/registerUser.js
@@ -1,16 +1,20 @@
+import bcrypt from "bcryptjs";
+
 import { User } from "../../models/index.js";
 import { ctrlWrapper } from "../../decorators/index.js";
 import HttpError from "../../helpers/HttpError.js";
 
 const registerUser = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashedPassword });
 
   res.status(201).json({
     user: { email: newUser.email, subscription: newUser.subscription },
