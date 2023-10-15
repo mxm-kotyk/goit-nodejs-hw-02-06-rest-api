d0631a5cfd003320d755d32056463612d5f87a48
Max Kotyk <mxm.kotyk@gmail.com>
2023-10-15 17:26:10 +0200

    Added pagination

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,7 +3,12 @@ import { ctrlWrapper } from "../../decorators/index.js";
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }).populate("owner", "email");
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "", { skip, limit }).populate(
+    "owner",
+    "email"
+  );
   res.json(result);
 };
 
