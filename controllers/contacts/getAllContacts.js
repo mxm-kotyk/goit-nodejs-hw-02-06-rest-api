dedd4a357935137140b350808273e2f092b94235
Max Kotyk <mxm.kotyk@gmail.com>
2023-10-15 19:33:39 +0200

    Added contacts filter by favorite

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,12 +3,22 @@ import { ctrlWrapper } from "../../decorators/index.js";
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "", { skip, limit }).populate(
-    "owner",
-    "email"
-  );
+
+  if (favorite) {
+    const result = await Contact.find({ owner, favorite }, "", {
+      skip,
+      limit,
+    }).populate("owner", "email");
+    res.json(result);
+    return;
+  }
+
+  const result = await Contact.find({ owner }, "", {
+    skip,
+    limit,
+  }).populate("owner", "email");
   res.json(result);
 };
 
